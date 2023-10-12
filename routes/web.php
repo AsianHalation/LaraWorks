42c9096e31a34b2537b03b9a4c4ddb59ddccdbe8
Don Kieu <2177651@talnet.nl>
2023-10-12 10:04:48 +0200

    welcome

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return (["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"]);
+    return view('welcome');
 });
